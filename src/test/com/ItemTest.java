ac3d8a3b7644a87f54157f9e338f76b1ad7a9857
Guadalupe Robles Gil <groblesgb@yahoo.com>
2012-09-20 22:40:57 -0500

    Word doc with Initial planning

---
+++ src/test/com/ItemTest.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: grobles
+ * Date: 20/09/12
+ * Time: 10:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ItemTest {
+}
