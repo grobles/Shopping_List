9d2665f4cdb4a827b1396dd7d2d9abc1e20df7cc
Brian Arnold <brianarnold789@gmail.com>
2012-10-14 05:36:48 -0400

    Testing Problems

---
+++ src/main/com/GUI/ValidateInput.java
@@ -0,0 +1,11 @@
+package com.GUI;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Brian
+ * Date: 10/14/12
+ * Time: 5:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ValidateImput {
+}
